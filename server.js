32ff2d9ad11d2c421cd135223cfbdff4a236218f
Sechaba Maso <sechabamaso@setups-MacBook-Pro.local>
2019-01-22 10:54:31 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+
+app.use((req,res,next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method}: ${req.url }`;
+    console.log(log);
+    fs.appendFile('server.log',log + '\n',(err) => {
+        if(err){
+            console.log("unable to write to file");
+        }
+    })
+    next();
+});
+
+
+
+hbs.registerHelper('getCurrentYear',() =>{
+    return new Date().getFullYear();
+});
+
+app.get('/',(request, response) => {
+    // response.send({
+    //     name: "Eldorado",
+    //     Likes: [{d:0,name:'delor'},{id:1, name: 'marcelos'}]
+    // });
+    response.render('home.hbs',{
+        title: "Home Side",
+        welcomeMessage:"totallt some fun",
+        copywrite: 2019
+    })
+});
+
+app.get('/about', (request,response)=>{
+    response.render('about.hbs', {
+        title: "about this page again but the title",
+    });
+});
+app.get('/bad',(request, response)=>{
+    response.send({
+        errorMessage: "unable to handle request"
+    });
+});
+app.use((req,res,next) => {
+    res.render('maintenance.hbs');
+});
+app.use(express.static(__dirname + '/public'));
+
+app.listen(3000, () => {
+    console.log("server s up now")
+});
